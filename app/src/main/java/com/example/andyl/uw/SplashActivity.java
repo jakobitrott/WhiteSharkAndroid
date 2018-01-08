9df70e951f7ab8be98e2a03712607806268385dd
LAPTOP-HDLUTTN7\andyl <andyle.uk13@gmail.com>
2018-01-08 11:34:39 +0000

    Splash Screen
    Opening the app will display a splash screen depicting utilitywise's logo which will redirect to the main menu afterwards.

---
+++ app/src/main/java/com/example/andyl/uw/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.andyl.uw;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Hide the status bar
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        else {
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        //display the logo during 1.5 seconds,
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
